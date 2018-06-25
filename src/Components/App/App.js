9c7d33db53bd22d52d0e89a56195f0591af33eb5
alexstouffer <bizkite@hotmail.com>
2018-06-24 18:03:29 -0700

    Completed steps 1-47 of 99 (Codecademy BWA Intensives). Error being thrown from step 34. this.props.tracks is undefined.

---
+++ src/Components/App/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      //searchResults & playlist Tracks should contain properties: name, artist, album, id
+      searchResults: [],
+      playlistName: 'New Playlist',
+      playlistTracks: []
+    };
+
+    this.addTrack = this.addTrack.bind(this);
+  }
+  addTrack(track) {
+    if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    }
+    let tracks = this.state.playlistTracks;
+    tracks.push(track);
+    this.setState({playlistTracks: tracks});
+  }
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults onAdd={this.addTrack} searchResults={this.state.searchResults} />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
